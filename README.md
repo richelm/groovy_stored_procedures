a2c75dd3eaf3d0bfdd66d31e3b7a54e6bc6df3f0
Shawn Rich <rich.shawn@gmail.com>
2016-06-01 11:06:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Groovy Stored Procedures
+
+Making use of [Groovy Sql class](http://docs.groovy-lang.org/latest/html/api/groovy/sql/Sql.html) can make calling stored procedures much easier in a Java environment. This project provides use cases that can be used for CRUD in a JSF application.
+
+1. Stored procedure with input parameter(s) and return value.
+1. Stored procedure with input parameter(s), return value and one return parameter.
+1. Stored procedure with input parameter(s), return value and one result set.
---
+++ groovy/test.groovy
@@ -0,0 +1,52 @@
+#!/usr/bin/env groovy
+
+import groovy.sql.Sql
+import groovy.json.*
+
+// define console for user input
+def cons = System.console()
+
+// load server connection properties
+Properties props = new Properties()
+File propsFile = new File('./application.properties')
+props.load(propsFile.newDataInputStream())
+String server = props.getProperty('server')
+String port = props.getProperty('port')
+String database = props.getProperty('database')
+String login = props.getProperty('login')
+
+def passwd = cons.readPassword("Password: ")
+
+sql = Sql.newInstance(
+	"jdbc:sybase:Tds:${server}:${port}/${database}?JCONNECT_VERSION=6&CHARSET=cp850",
+	"${login}",
+	"${passwd}",
+	"com.sybase.jdbc3.jdbc.SybDriver")
+
+String storedProcedureCall
+def params
+int returnValue
+int colorIndex
+
+println("Testing stored procedure up_raise_error")
+def mynum = cons.readLine("myNum: ")
+
+storedProcedureCall = "{? = call up_raise_error(?)}"
+params = [Sql.INTEGER,mynum.toInteger()]
+sql.call(storedProcedureCall, params) {rv ->
+  returnValue = rv
+}
+println("returnValue: ${returnValue}")
+
+
+println("Testing stored procedure up_multi_params")
+def myColor = cons.readLine("myColor: ")
+
+storedProcedureCall = "{? = call up_multi_params(?,?)}"
+params = [Sql.INTEGER,myColor,Sql.INTEGER]
+sql.call(storedProcedureCall, params) {rv,ci ->
+  returnValue = rv
+  colorIndex = ci
+}
+println("returnValue: ${returnValue}")
+println("colorIndex: ${colorIndex}")
