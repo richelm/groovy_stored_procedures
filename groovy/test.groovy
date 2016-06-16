a313de45d672f6697cdcd5533adffabce5079f24
Shawn Rich <rich.shawn@gmail.com>
2016-06-16 11:52:45 -0400

    Testing sp calls

--- groovy/test.groovy
+++ groovy/test.groovy
@@ -22,40 +22,60 @@ def params
 int returnValue
 int colorIndex
 
-println("\nTesting stored procedure up_raise_error")
-def mynum = cons.readLine("myNum: ")
-
-storedProcedureCall = "{? = call up_raise_error(?)}"
-params = [Sql.INTEGER,mynum.toInteger()]
-
-try {
-  sql.call(storedProcedureCall, params) {rv ->
-    returnValue = rv
-  }
-  println("returnValue: ${returnValue}")
-} catch (Exception e) {
-  String errMessage = e.getMessage()
-  println errMessage
-}
-
-
-println("\nTesting stored procedure up_multi_params")
-def myColor = cons.readLine("myColor: ")
-
-storedProcedureCall = "{? = call up_multi_params(?,?)}"
-params = [Sql.INTEGER,myColor,Sql.INTEGER]
-sql.call(storedProcedureCall, params) {rv,ci ->
-  returnValue = rv
-  colorIndex = ci
-}
-println("returnValue: ${returnValue}")
-println("colorIndex: ${colorIndex}")
+// println("\nTesting stored procedure up_raise_error")
+// def mynum = cons.readLine("myNum: ")
+//
+// storedProcedureCall = "{? = call up_raise_error(?)}"
+// params = [Sql.INTEGER,mynum.toInteger()]
+//
+// try {
+//   sql.call(storedProcedureCall, params) {rv ->
+//     returnValue = rv
+//   }
+//   println("returnValue: ${returnValue}")
+// } catch (Exception e) {
+//   String errMessage = e.getMessage()
+//   println errMessage
+// }
 
 
-// println("\nTesting stored procedure up_retrieve_employee")
+// println("\nTesting stored procedure up_create_employee")
+//
+// storedProcedureCall = "{? call up_create_employee(?,?,?,?,?,?)}"
+// params = [Sql.INTEGER,'Shawn2','Richs','richs2@msu.edu','6/1/2016',10,1.5]
+//
+// try {
+//   sql.call(storedProcedureCall, params) {rv ->
+//     returnValue = rv
+//   }
+//   println("returnValue: ${returnValue}")
+// } catch (Exception e) {
+//   String errMessage = e.getMessage()
+//   println errMessage
+// }
+
+
+// println("\nTesting stored procedure up_multi_params")
+// def myColor = cons.readLine("myColor: ")
+//
+// storedProcedureCall = "{? = call up_multi_params(?,?)}"
+// params = [Sql.INTEGER,myColor,Sql.INTEGER]
+// sql.call(storedProcedureCall, params) {rv,ci ->
+//   returnValue = rv
+//   colorIndex = ci
+// }
+// println("returnValue: ${returnValue}")
+// println("colorIndex: ${colorIndex}")
+
+println("\nTesting stored procedure up_retrieve_employee")
 def empid = cons.readLine("Employee ID: ")
 
-storedProcedureCall = "execute up_retrieve_employee ?"
-params = [empid.toInteger()]
-def rows = sql.rows(storedProcedureCall, params)
-println rows.join('\n')
+storedProcedureCall = "execute ? = up_retrieve_employee ?"
+params = [Sql.INTEGER, empid.toInteger()]
+def rows = sql.execute(storedProcedureCall, params) {rv,rows ->
+  returnValue = rv
+  println rows
+}
+//println returnValue
+// println rows.join('\n')
+//println rows
